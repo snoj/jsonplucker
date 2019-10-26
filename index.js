4d6ea768448f91337a185e82f0e17bf8d6dbc55f
Josh Erickson <josh@snoj.us>
2019-10-26 01:55:32 -0500

    Fixes and Flags
    Fixed issues with multiline json files
    Added input flags
      -i, --input <jsonfile path>
      -p don't print key name

--- index.js
+++ index.js
@@ -1,64 +1,63 @@
-var _ = require('lodash');
+const commander = require('commander'),
+      _ = require('lodash'),
+      fs = require('fs');
+
 _.mixin(require('lodash-flatkeystree'));
 
-(function() {
-  var tryjson = function(teststring) {
-    try {
-      return JSON.parse(teststring);
-    } catch(ex) {
-      return false;
+const opts = commander.version('0.3.0')
+  .option('-i, --input <jsonfile>', 'JSON file input')
+  .option('-p', 'Don\'t print json keys', false)
+  .parse(process.argv);
+
+function tryjson(teststring) {
+  try {
+    return JSON.parse(teststring);
+  } catch(ex) {
+    return false;
+  }
+};
+
+function parse(json, paths) {
+  if(!!!json) {
+    console.error(json);
+    throw "Invalid json";
+  }
+
+  let rg = /^\/(.+)\/([gi]*|)$/;
+
+  let keys = [];
+  let rkeys = [];
+
+  _.each(paths, function(p) {
+    if(rg.test(p)) {
+      let breakdown = p.match(rg);
+      let r = new RegExp(breakdown[1], breakdown[2]);
+      rkeys.push(r);
+    } else {
+      keys.push(p);
     }
-  };
-  var finished = false;
-  var finish = function(obj) {
-    if(!!finished) return;
+  });
 
-    finished = true;
-
-    if(!!!obj) {
-      throw "Invalid json string, cannot be parsed.";
-    }
-    var paths = _.slice(process.argv, 2);
-    var rg = /^\/(.+)\/([gi]*|)$/;
-
-    var keys = [];
-    var rkeys = [];
-
-    _.each(paths, function(p) {
-      if(rg.test(p)) {
-        var breakdown = p.match(rg);
-        var r = new RegExp(breakdown[1], breakdown[2]);
-        rkeys.push(r);
-      } else {
-        keys.push(p);
+  let values = _.at(json, keys);
+  _.each(values, function(v, index) {
+    console.log.apply(null, [paths[index], v].slice(!!opts.P));
+  });
+  let keytree = _.keysDeep(json);
+  _.each(rkeys, function(rk) {
+    _.each(keytree, function(k) {
+      if(rk.test(k)) {
+        console.log.apply(null, [k, _.get(json, k)].slice(!!opts.P));
       }
     });
-    var values = _.at(obj, keys);
-    _.each(values, function(v, index) {
-      console.log(paths[index], v);
-    });
-    var keytree = _.keysDeep(obj);
-    _.each(rkeys, function(rk) {
-      _.each(keytree, function(k) {
-        if(rk.test(k)) {
-          console.log(k, _.get(obj, k));
-        }
-      });
-    });
-  };
-  var raw_string = "";
-  process.stdin.setEncoding('utf8');
-  process.stdin.on("readable", function() {
-    var tmpchunk = process.stdin.read();
-    var attempt = tryjson(tmpchunk);
-    if(!!attempt)
-      finish(attempt);
-    else
-      raw_string += tmpchunk;
   });
+}
 
-  process.stdin.on('end', function() {
-    finish(tryjson(raw_string));
-  });
-
+(async function() {
+  if(opts.input) {
+    parse(JSON.parse(fs.readFileSync(opts.input)), opts.args);
+    return;
+  } else {
+    let stdinBuffer = fs.readFileSync(0);
+    parse(JSON.parse(stdinBuffer.toString()), opts.args);
+  }
 })();
